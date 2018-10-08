f617fed29aa03c444df9f36c1950273950d04f8a
Mark Mandell <markman0922@gmail.com>
2018-10-08 12:10:21 -0700

    adds memories.html/links to home page

--- server.js
+++ server.js
@@ -7,6 +7,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require('./models');
 
+
+// allow cross origin requests (optional)
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('public'));
+
+app.get('/index', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/memories', (req, res) => {
+  res.sendFile(__dirname + '/views/memories.html');
+});
+
 // Get all memories (json data) at this endpoint
 app.get('/api/memories', (req, res) => {
   db.Memory.find((err, allMemories) => {
@@ -23,19 +42,6 @@ app.get('/api/users', (req, res) => {
   });
 });
 
-// allow cross origin requests (optional)
-// https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Express server is up and running on http://localhost:3000/');
