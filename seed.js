d3ec79f973d3008db474be3d11ebc54d3b5d9861
Mark Mandell <markman0922@gmail.com>
2018-10-08 19:34:44 -0700

    renders memories data to front end

--- seed.js
+++ seed.js
@@ -1,89 +1,49 @@
 const db = require('./models');
 
-// const new_user = [
-//   {
-//     userName: 'Praveen',
-//     password: 'password',
-//     profilePic: 'URL',
-//     age: 29
-//   },
-//   {
-//     userName: 'Tirapat',
-//     password: 'password2',
-//     profilePic: 'URL',
-//     age: 29
-//   },
-//   {
-//     userName: 'Mark',
-//     password: 'password3',
-//     profilePic: 'URL',
-//     age: 24
-//   },
-// ]
-//
-// const new_memory = [
-//   {
-//     title: 'Dr. ',
-//     name: 'Tirapat',
-//     description: 'Moved from New York City!',
-//     image: 'URL',
-//     date: 'September 1st, 2018',
-//   },
-//   {
-//     title: 'Professor ',
-//     name: 'Mark',
-//     description: 'Moved from South Florida!',
-//     image: 'URL',
-//     date: 'September 5th, 2018',
-//   },
-//   {
-//     title: 'Mr. ',
-//     name: 'Praveen',
-//     description: ' From East Bay!',
-//     image: 'URL',
-//     date: 'September 1st, 2014',
-//   },
-// ]
-//
-// db.User.deleteMany({}, (err, users) => {
-//   if(err) throw err;
-//   console.log(`Users deleted`);
-//
-//   db.User.create(new_user, (err, users) => {
-//     if (err) throw err;
-//     console.log(`User created: ${users}`);
-//   });
-// });
-//
-// db.Memory.deleteMany({}, (err, memories) => {
-//   if(err) throw err;
-//   console.log(`Memories deleted`);
-//
-//   db.Memory.create(new_memory, (err, memories) => {
-//     if(err) throw err;
-//     console.log(`New memories saved: ${memories}`);
-//     process.exit();
-//   });
-// });
-
-var newUser =
+var newUser = [
   {
     userName: 'Tirapat',
-    password: 'password2',
-    profilePic: 'URL',
+    password: 'password1',
+    profilePic: 'https://i.imgur.com/zE9ezSK.png',
     age: 29
-  }
+  },
+  {
+    userName: 'Mark',
+    password: 'password3',
+    profilePic: 'https://i.imgur.com/33ZErzy.jpg',
+    age: 24
+  },
+  {
+    userName: 'Praveen',
+    password: 'password2',
+    profilePic: 'https://i.imgur.com/jf9US6M.png',
+    age: 24
+  },
+];
 
-
-var newMemory =
+var newMemory = [
   {
     title: 'Dr. ',
     name: 'Tirapat',
     description: 'Moved from New York City!',
-    image: 'URL',
+    image: 'https://i.imgur.com/zE9ezSK.png',
     date: 'September 1st, 2018',
-  }
-
+  },
+  {
+    title: 'Mr. ',
+    name: 'Mark',
+    description: 'Moved from sweaty South Florida!',
+    image: 'https://i.imgur.com/33ZErzy.jpg',
+    date: 'September 5th, 2018',
+  },
+  {
+    title: 'Mr. ',
+    name: 'Praveen',
+    description: 'From East Bay!',
+    image: 'https://i.imgur.com/jf9US6M.png',
+    date: 'September 1st, 2014',
+  },
+];
 
 db.Memory.deleteMany({}, (err, deletedMemories) => {
   if (err) throw err;
@@ -91,18 +51,24 @@ db.Memory.deleteMany({}, (err, deletedMemories) => {
   db.User.deleteMany({}, (err, deletedUsers) => {
     if (err) throw err;
     console.log('Deleted users')
-    db.Memory.create(newMemory, (err, savedMemory) => {
-      if (err) throw err;
-      console.log(`Created new memory: ${savedMemory}`)
-      db.User.create(newUser, (err, savedUser) => {
+    newMemory.forEach((newMemory) => {
+      db.Memory.create(newMemory, (err, savedMemory) => {
         if (err) throw err;
-        console.log(`New user created: ${savedUser}`)
-        savedMemory.user = savedUser;
-        // savedUser.markModified(savedMemory.user);
-        savedMemory.save((err, updatedUser) => {
-          process.exit();
-        });
+        console.log(`Created new memory: ${savedMemory}`)
+        newUser.forEach((newUser) => {
+          db.User.create(newUser, (err, savedUser) => {
+            if (err) throw err;
+            console.log(`New user created: ${savedUser}`)
+            if(savedUser.userName === savedMemory.name) {
+              savedMemory.user = savedUser;
+              savedMemory.save((err, updatedUser) => {
+
+              });
+              // process.exit();
+            }
+          });
+        })
       });
-    });
+    })
   })
 })
